7b3d71ca83e6a181421078f12add67ab1825bd33
aagorbachev <69871856+aagorbachev@users.noreply.github.com>
2020-10-31 06:49:02 +0300

    Урок 4

--- script/lesson3/exercise2.js
+++ script/lesson3/exercise2.js
@@ -2,6 +2,12 @@
 // a) Организовать такой массив для хранения товаров в корзине;
 // b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
 
+// let arr = [1, 2, 3, 4, 5];
+
+//let result = arr.reduce((sum, current) => sum + current, 0);
+
+
+
 let basket = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function countBasketPrice(basket) {
---
+++ script/lesson4/exercise2.js
@@ -0,0 +1,70 @@
+// 2.Продолжить работу с интернет - магазином:
+
+// В прошлом домашнем задании вы реализовали корзину на базе массивов.Какими объектами можно заменить их элементы ?
+//     Реализуйте такие объекты.
+// Перенести функционал подсчета корзины на объектно - ориентированную базу.
+
+// На дополнительный плюс - реализовать класс корзины.Свойство - сама корзина(массив).Методы - подсчет стоимости, добавление товара в корзину.
+
+//
+
+class cart {
+    products = []
+    totalPrice = 0
+    totalQuantity = 0
+    countTotalPrice() {
+        let current = this.products.reduce((sum, current) => sum + current, 0)
+        this.totalPrice = current
+        return this.totalPrice
+    }
+    countTotalQuantity() {
+        this.totalQuantity = this.products.length
+        return this.totalQuantity
+    }
+    addToCart(product, quantity) {
+        let list = product.buy(quantity)
+        if (list === null) {
+            console.log("К сожалению, товаров в нужном количестве не имеется в наличии")
+        }
+        else {
+            list.forEach(element => {
+                this.products.push(element)
+            });
+            this.countTotalPrice()
+            this.countTotalQuantity()
+        }
+    }
+}
+
+class product {
+    constructor(name, price, quantity) {
+        this.name = name
+        this.price = price
+        this.quantity = quantity
+    }
+    buy(quantity) {
+        if (quantity > this.quantity) {
+            return null
+        }
+        else {
+            this.quantity = this.quantity - quantity;
+            let list = [];
+            for (; quantity > 0; quantity--) {
+                list.push(this.price)
+            }
+            return list
+        }
+    }
+}
+
+const TestCart = new cart
+console.log(TestCart)
+const product1 = new product("Iphone 10", 800, 2)
+TestCart.addToCart(product1, 5)
+
+console.log(TestCart)
+console.log(product1)
+
+// 1. В функцию addToCart добавить проверку на наличие товара
+// 2. Реализовать подсчет стоимости и количества + добавление имени товара
+
